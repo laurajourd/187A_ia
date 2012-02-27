d9ee4f571077b6b2f54822fecd5f93f6cd0b820a
Rachel C <rcusing@ucsd.edu>
2012-02-26 16:42:46 -0800

    edited about wireframe

--- About_UL_Lafayette_files/axurerp_pagespecificscript.js
+++ About_UL_Lafayette_files/axurerp_pagespecificscript.js
@@ -47,144 +47,40 @@ function PopulateVariables(value) {
 
 function OnLoad(e) {
 
-if (true) {
-
-	self.location.href="#" + GetQuerystring();
-
-}
-else
-if (true) {
-
-	self.location.href="Admission.html" + GetQuerystring();
-
 }
 
-}
-
-var u50 = document.getElementById('u50');
-gv_vAlignTable['u50'] = 'top';
-var u51 = document.getElementById('u51');
-gv_vAlignTable['u51'] = 'top';
-var u52 = document.getElementById('u52');
-gv_vAlignTable['u52'] = 'top';
-var u53 = document.getElementById('u53');
-gv_vAlignTable['u53'] = 'top';
-var u20 = document.getElementById('u20');
-
-var u21 = document.getElementById('u21');
-gv_vAlignTable['u21'] = 'center';
-var u22 = document.getElementById('u22');
-
-var u23 = document.getElementById('u23');
-gv_vAlignTable['u23'] = 'center';
-var u24 = document.getElementById('u24');
-
-var u25 = document.getElementById('u25');
-gv_vAlignTable['u25'] = 'center';
-var u26 = document.getElementById('u26');
-gv_vAlignTable['u26'] = 'top';
-var u27 = document.getElementById('u27');
+var u31 = document.getElementById('u31');
+gv_vAlignTable['u31'] = 'center';
+var u36 = document.getElementById('u36');
+gv_vAlignTable['u36'] = 'center';
+var u16 = document.getElementById('u16');
+gv_vAlignTable['u16'] = 'center';
+var u17 = document.getElementById('u17');
 
 var u28 = document.getElementById('u28');
 gv_vAlignTable['u28'] = 'center';
 var u29 = document.getElementById('u29');
 gv_vAlignTable['u29'] = 'top';
-var u30 = document.getElementById('u30');
-
-var u31 = document.getElementById('u31');
-gv_vAlignTable['u31'] = 'center';
-var u32 = document.getElementById('u32');
-
-var u33 = document.getElementById('u33');
-
-var u34 = document.getElementById('u34');
-gv_vAlignTable['u34'] = 'center';
-var u35 = document.getElementById('u35');
-
-var u36 = document.getElementById('u36');
-gv_vAlignTable['u36'] = 'center';
-var u37 = document.getElementById('u37');
-gv_vAlignTable['u37'] = 'top';
-var u38 = document.getElementById('u38');
-gv_vAlignTable['u38'] = 'top';
-var u39 = document.getElementById('u39');
-
-var u0 = document.getElementById('u0');
-
-var u1 = document.getElementById('u1');
-gv_vAlignTable['u1'] = 'center';
-var u2 = document.getElementById('u2');
-
-var u3 = document.getElementById('u3');
-gv_vAlignTable['u3'] = 'center';
-var u4 = document.getElementById('u4');
-
-var u5 = document.getElementById('u5');
-gv_vAlignTable['u5'] = 'center';
-var u6 = document.getElementById('u6');
-gv_vAlignTable['u6'] = 'top';
-var u7 = document.getElementById('u7');
-
 var u8 = document.getElementById('u8');
 gv_vAlignTable['u8'] = 'center';
-var u9 = document.getElementById('u9');
+var u30 = document.getElementById('u30');
 
-var u42 = document.getElementById('u42');
+var u21 = document.getElementById('u21');
+gv_vAlignTable['u21'] = 'center';
+var u6 = document.getElementById('u6');
+gv_vAlignTable['u6'] = 'top';
+var u32 = document.getElementById('u32');
 
-u42.style.cursor = 'pointer';
-if (bIE) u42.attachEvent("onclick", Clicku42);
-else u42.addEventListener("click", Clicku42, true);
-function Clicku42(e)
-{
-windowEvent = e;
+var u15 = document.getElementById('u15');
 
+var u13 = document.getElementById('u13');
 
-if (true) {
+var u14 = document.getElementById('u14');
+gv_vAlignTable['u14'] = 'center';
+var u4 = document.getElementById('u4');
 
-	self.location.href="resources/reload.html#" + encodeURI(PageUrl + GetQuerystring());
-
-}
-
-}
-gv_vAlignTable['u42'] = 'top';
-var u40 = document.getElementById('u40');
-gv_vAlignTable['u40'] = 'center';
-var u41 = document.getElementById('u41');
-
-u41.style.cursor = 'pointer';
-if (bIE) u41.attachEvent("onclick", Clicku41);
-else u41.addEventListener("click", Clicku41, true);
-function Clicku41(e)
-{
-windowEvent = e;
-
-
-if (true) {
-
-	self.location.href="homepage_redesign.html" + "";
-
-}
-
-}
-gv_vAlignTable['u41'] = 'top';
-var u45 = document.getElementById('u45');
-
-u45.style.cursor = 'pointer';
-if (bIE) u45.attachEvent("onclick", Clicku45);
-else u45.addEventListener("click", Clicku45, true);
-function Clicku45(e)
-{
-windowEvent = e;
-
-
-if (true) {
-
-	self.location.href="Campus_Life.html" + GetQuerystring();
-
-}
-
-}
-gv_vAlignTable['u45'] = 'top';
+var u38 = document.getElementById('u38');
+gv_vAlignTable['u38'] = 'top';
 var u43 = document.getElementById('u43');
 
 u43.style.cursor = 'pointer';
@@ -221,24 +117,74 @@ if (true) {
 
 }
 gv_vAlignTable['u44'] = 'top';
-var u48 = document.getElementById('u48');
+var u40 = document.getElementById('u40');
+gv_vAlignTable['u40'] = 'center';
+var u1 = document.getElementById('u1');
+gv_vAlignTable['u1'] = 'center';
+var u37 = document.getElementById('u37');
+gv_vAlignTable['u37'] = 'top';
+var u26 = document.getElementById('u26');
+gv_vAlignTable['u26'] = 'top';
+var u41 = document.getElementById('u41');
 
-u48.style.cursor = 'pointer';
-if (bIE) u48.attachEvent("onclick", Clicku48);
-else u48.addEventListener("click", Clicku48, true);
-function Clicku48(e)
+u41.style.cursor = 'pointer';
+if (bIE) u41.attachEvent("onclick", Clicku41);
+else u41.addEventListener("click", Clicku41, true);
+function Clicku41(e)
 {
 windowEvent = e;
 
 
 if (true) {
 
-	self.location.href="http://youtube.com/user/OffiicialULLafayette" + "";
+	self.location.href="homepage_redesign.html" + "";
 
 }
 
 }
+gv_vAlignTable['u41'] = 'top';
+var u10 = document.getElementById('u10');
+gv_vAlignTable['u10'] = 'center';
+var u11 = document.getElementById('u11');
 
+var u3 = document.getElementById('u3');
+gv_vAlignTable['u3'] = 'center';
+var u12 = document.getElementById('u12');
+gv_vAlignTable['u12'] = 'center';
+var u39 = document.getElementById('u39');
+
+var u9 = document.getElementById('u9');
+
+var u35 = document.getElementById('u35');
+
+var u27 = document.getElementById('u27');
+
+var u7 = document.getElementById('u7');
+
+var u42 = document.getElementById('u42');
+
+u42.style.cursor = 'pointer';
+if (bIE) u42.attachEvent("onclick", Clicku42);
+else u42.addEventListener("click", Clicku42, true);
+function Clicku42(e)
+{
+windowEvent = e;
+
+
+if (true) {
+
+	self.location.href="resources/reload.html#" + encodeURI(PageUrl + GetQuerystring());
+
+}
+
+}
+gv_vAlignTable['u42'] = 'top';
+var u23 = document.getElementById('u23');
+gv_vAlignTable['u23'] = 'center';
+var u24 = document.getElementById('u24');
+
+var u25 = document.getElementById('u25');
+gv_vAlignTable['u25'] = 'center';
 var u46 = document.getElementById('u46');
 
 u46.style.cursor = 'pointer';
@@ -257,28 +203,76 @@ if (true) {
 
 }
 gv_vAlignTable['u46'] = 'top';
-var u47 = document.getElementById('u47');
-gv_vAlignTable['u47'] = 'top';
-var u49 = document.getElementById('u49');
-gv_vAlignTable['u49'] = 'center';
-var u10 = document.getElementById('u10');
-gv_vAlignTable['u10'] = 'center';
-var u11 = document.getElementById('u11');
+var u53 = document.getElementById('u53');
+gv_vAlignTable['u53'] = 'top';
+var u56 = document.getElementById('u56');
+gv_vAlignTable['u56'] = 'top';
+var u54 = document.getElementById('u54');
 
-var u12 = document.getElementById('u12');
-gv_vAlignTable['u12'] = 'center';
-var u13 = document.getElementById('u13');
-
-var u14 = document.getElementById('u14');
-gv_vAlignTable['u14'] = 'center';
-var u15 = document.getElementById('u15');
-
-var u16 = document.getElementById('u16');
-gv_vAlignTable['u16'] = 'center';
-var u17 = document.getElementById('u17');
+var u2 = document.getElementById('u2');
 
 var u18 = document.getElementById('u18');
 
 var u19 = document.getElementById('u19');
 gv_vAlignTable['u19'] = 'center';
+var u55 = document.getElementById('u55');
+gv_vAlignTable['u55'] = 'center';
+var u20 = document.getElementById('u20');
+
+var u5 = document.getElementById('u5');
+gv_vAlignTable['u5'] = 'center';
+var u48 = document.getElementById('u48');
+
+u48.style.cursor = 'pointer';
+if (bIE) u48.attachEvent("onclick", Clicku48);
+else u48.addEventListener("click", Clicku48, true);
+function Clicku48(e)
+{
+windowEvent = e;
+
+
+if (true) {
+
+	self.location.href="http://youtube.com/user/OffiicialULLafayette" + "";
+
+}
+
+}
+
+var u22 = document.getElementById('u22');
+
+var u49 = document.getElementById('u49');
+gv_vAlignTable['u49'] = 'center';
+var u47 = document.getElementById('u47');
+gv_vAlignTable['u47'] = 'top';
+var u50 = document.getElementById('u50');
+gv_vAlignTable['u50'] = 'top';
+var u51 = document.getElementById('u51');
+gv_vAlignTable['u51'] = 'top';
+var u45 = document.getElementById('u45');
+
+u45.style.cursor = 'pointer';
+if (bIE) u45.attachEvent("onclick", Clicku45);
+else u45.addEventListener("click", Clicku45, true);
+function Clicku45(e)
+{
+windowEvent = e;
+
+
+if (true) {
+
+	self.location.href="Campus_Life.html" + GetQuerystring();
+
+}
+
+}
+gv_vAlignTable['u45'] = 'top';
+var u52 = document.getElementById('u52');
+gv_vAlignTable['u52'] = 'top';
+var u33 = document.getElementById('u33');
+
+var u34 = document.getElementById('u34');
+gv_vAlignTable['u34'] = 'center';
+var u0 = document.getElementById('u0');
+
 if (window.OnLoad) OnLoad();
